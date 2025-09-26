44b14ad1f8d7f52a9115a43796018010bb3dbe9d
elliotjsimp <elliotsimpson2006@gmail.com>
2025-09-26 16:45:25 -0700

    Clarified 1 or 11 for Ace logic.

--- deck.py
+++ deck.py
@@ -1,18 +1,14 @@
 class Card:
     """Represents a single playing card."""
-    
     def __init__(self, rank: str, suit: str):
         self.rank = rank  # e.g., "2", "J", "A"
         self.suit = suit  # e.g., "Hearts", "Spades"
 
-    # Used in round.py
     def __str__(self):
         return f"{self.rank}{self.suit}"
     
-    
     def __repr__(self):
         return f"{self.rank}"
-    
     
     @property
     def value(self) -> int:
@@ -20,11 +16,17 @@ class Card:
         if self.rank in ["J", "Q", "K"]:
             return 10
         elif self.rank == "A":
-            return 11  # or 1 depending on hand logic
+            return 11  # switches to 1 in hand_total() if necessary.
         else:
             return int(self.rank)
+    
+    @property
+    def hilo_value(self) -> int:
+        "Returns the Hi-Lo value of a card."
+        if self.value in [2,3,4,5,6]: return 1
+        elif self.value in [10, 11]: return -1
+        return 0
 
-        
 class Deck:
     """Represents a deck of cards."""
 
--- hand.py
+++ hand.py
@@ -3,8 +3,7 @@ from deck import Card
 class Hand:
     def __init__(self):
         self.cards: list[Card] = []
-        self.bet: int
-                               
+        self.bet: int         
 
     def hand_total(self) -> int:
         """Compute hand value considering Aces as 1 or 11."""
@@ -13,28 +12,23 @@ class Hand:
 
         for card in self.cards: 
             if card.rank == "A":
-                total += 11
+                total += card.value # Consider as 11
                 ace_count += 1
             else:
                 total += card.value
 
         while total > 21 and ace_count > 0:
-            total -= 10
+            total -= 10 # Consider as 1
             ace_count -= 1
 
         return total
-    
 
     def add_card(self, card: Card) -> None:
         self.cards.append(card)
-
     
     def is_pair(self) -> bool:
         return len(self.cards) == 2 and self.cards[0].value == self.cards[1].value
-    
 
     def has_busted(self) -> bool:
         """Has this hand gone over 21?"""
         return self.hand_total() > 21
-            
-
